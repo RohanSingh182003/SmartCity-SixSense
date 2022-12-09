a4b51343830979602d12006ac480bcde5386b358
Rohan SIngh <rs7289579@gmail.com>
2022-12-09 18:46:06 +0530

    optimize 404 page

--- src/components/404/PageNotFound.jsx
+++ src/components/404/PageNotFound.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 const PageNotFound = () => {
   return (
     <div className="min-h-screen w-screen grid place-items-center bg-gradient-to-r from-cyan-500 to-pink-500">
-        <p className="text-4xl text-white">404 Page Not Found</p>
+      <div className="bg-white w-96 h-36 rounded-sm grid place-items-center">
+        <p className="text-4xl text-gray-900">Error 404</p>
+        <p className='text-gray-500'>Page Not Found</p>
+        </div>
     </div>
   )
 }
